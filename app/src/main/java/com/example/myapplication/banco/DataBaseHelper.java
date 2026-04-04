e8bd227e7465b513a36d8eedcdaf2323ce24ca47
Carol <carolportellazanelato@gmail.com>
2026-04-03 21:36:48 -0300

    Update DataBaseHelper.java

--- app/src/main/java/com/example/myapplication/banco/DataBaseHelper.java
+++ app/src/main/java/com/example/myapplication/banco/DataBaseHelper.java
@@ -1,10 +1,13 @@
 package com.example.myapplication.banco;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
+
+import com.example.myapplication.model.MainQuestion;
 
 public class DataBaseHelper extends SQLiteOpenHelper {
 
@@ -22,11 +25,37 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         // CRIANDO A TABELA //
-        String createTableStatement = "CREATE TABLE " + tbMainQ + " (" + column_id + " INTEGER PRIMARY KEY AUTOINCREMENT, ");
+        String createTableStatement = "CREATE TABLE " + tbMainQ + " (" + column_id + " INTEGER PRIMARY KEY AUTOINCREMENT, " + column_texto + " VARCHAR(100), " + column_exemplo + " VARCHAR(100))";
+        db.execSQL(createTableStatement);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+    }
+
+    public void addPeerguntas(){
+        MainQuestion main1 = new MainQuestion(1, "TESTE", "TESTE");
+
+        System.out.println(add(main1));
+    }
+
+    public boolean add(MainQuestion mainQ){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues cv = new ContentValues();
+        cv.put(column_id, mainQ.getId());
+        cv.put(column_texto, mainQ.getTexto());
+        cv.put(column_exemplo, mainQ.getExemplo());
+
+        long insert = db.insert(tbMainQ, null, cv);
+
+        if(insert == -1){
+            return false;
+        }
+
+        else{
+            return true;
+        }
     }
 }
